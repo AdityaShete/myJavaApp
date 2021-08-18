5cebbd3fb9c605d7e34c67884397e9a6c4b44eec
AdityaShete <adityashete009@gmail.com>
2021-08-18 16:17:26 +0530

    A second minor commit

--- MyJavaApp/src/com/ofss/HelloWorld.java
+++ MyJavaApp/src/com/ofss/HelloWorld.java
@@ -1,9 +1,13 @@
 package com.ofss;
 
 public class HelloWorld {
+	public static int sum(int a, int b) {
+		return a+b;
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Hello GitHub");
+		System.out.println(sum(7,4));
 	}
 
 }
